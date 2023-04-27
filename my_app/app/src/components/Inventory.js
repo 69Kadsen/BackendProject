d392f30f86081e5041304ee9ce1938121457b6e8
69Kadsen <129136214+69Kadsen@users.noreply.github.com>
2023-04-27 18:23:29 +0000

    removal of pyc files

--- my_app/app/src/components/Inventory.js
+++ my_app/app/src/components/Inventory.js
@@ -1,14 +1,36 @@
 import React, { useEffect, useState } from "react";
 
-import User from "./User";
-
 
 const Inventory = () => {
 
-    console.log("sup", User)
-
-    const [inventory, setInventory] = useState(User.inventory);
+    const [user, setUser] = useState();
+    const [inventory, setInventory] = useState([]);
     const [newItem, setNewItem] = useState({});
+
+    useEffect( () => {
+        async function getCurrentUser() {
+            const token = localStorage.getItem("token");
+            const requestOptions = {
+                method: "GET",
+                headers: {"token": token}
+            }
+  
+            const response = await fetch('https://69kadsen-glorious-memory-5wv4wwxj6p6cpxr6-8000.preview.app.github.dev/api/users/me', requestOptions);
+            if (response.ok) {
+                const userData = await response.json();
+                setUser(userData);
+                setInventory(userData.inventory);
+                console.log(userData)
+            }
+        }
+        getCurrentUser();
+    }, []);
+  
+  
+    if (!user) {
+      console.log("User data not found"); // for debugging
+      return <div>Loading user data...</div>;
+    }
 
 
     const handleInputChange = (event) => {
@@ -19,10 +41,12 @@ const Inventory = () => {
     const handleSubmit = (event) => {
         event.preventDefault();
         setInventory([...inventory, newItem]);
+        addInventoryItem(newItem)
         setNewItem({});
       };
 
     const addInventoryItem = async (item) => {
+        console.log(item) // Debug
         const token = localStorage.getItem("token");
         const requestOptions = {
             method: "PUT",
@@ -38,16 +62,26 @@ const Inventory = () => {
         }
     };
 
-    const inventoryList = inventory.map((item, index) => (
-        <li key={index}>{item}</li>
-    ));
+    const values = Object.values(inventory);
+    console.log(values)
 
+    if (user.inventory) {
+
+        const inventoryList = Array.isArray(inventory) ? inventory.map((item, index) => (
+            <>
+            <li key={index}>{item.bot.name}</li>
+            <li key={index}>{item.bot.number}</li>
+            <div>I am something</div>
+            <button>Try me</button>
+            </>
+
+        )) : null;
 
     return (
         <>
             <h2>Inventory</h2>
             <ul>{inventoryList}</ul>
-
+            <h2>Add entry:</h2>
             <form onSubmit={handleSubmit}>
                 <label>
                 Bot Number:
@@ -83,6 +117,7 @@ const Inventory = () => {
 
         </>
     );
+    };
 };
 
 
--- my_app/app/src/components/Profile.js
+++ my_app/app/src/components/Profile.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Profile = () => {
 
@@ -15,7 +15,6 @@ const Profile = () => {
           const response = await fetch('https://69kadsen-glorious-memory-5wv4wwxj6p6cpxr6-8000.preview.app.github.dev/api/users/me', requestOptions);
           if (response.ok) {
               const userData = await response.json();
-              console.log("userData:", userData); // for debugging
               setUser(userData);
           }
       }
@@ -28,14 +27,12 @@ const Profile = () => {
     return <div>Loading user data...</div>;
   }
 
+  console.log(user)
+
   return (
     <div>
       <h2>Welcome, {user.username}!</h2>
-      <img src="" alt="Profile" />
       <p>Email: {user.email}</p>
-      <p>Bot number: {user.inventory.bot_number}</p>
-      <p>bot_name: {user.inventory.bot.name}</p>
-
     </div>
   );
 };
